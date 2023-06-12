fda104d65311d4355b895e56b0cdfec59ba1872f
exa <akun9396@gmail.com>
2023-06-12 14:51:23 +0700

    update

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -3,34 +3,29 @@ import React, { Component } from 'react'
 import Slider from 'react-slick'
 
 const Testimony = () => {
-  const next = () => {}
-  const prev = () => {}
-
   const settings = {
-    dots: true,
-    arrows: false,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: next(),
-    prevArrow: <Prev />,
   }
   return (
-    <section id="testimony" className="my-5 mx-auto">
-      <h1 className="text-center font-bold text-2xl">TESTIMONY</h1>
-      <div className="items-center">
-        <Slider {...settings}>
-          {testimony.map((testi, index) => {
-            const { image, name, desc } = testi
-            return (
-              <div key={index} className="inline mx-auto text-center text-xl p-8  max-w-xl rounded-xl shadow-xl ">
-                <img src={image} alt={name} className="w-56 block mx-auto" />
-                <p>{name}</p>
-                <p>"{desc}"</p>
-              </div>
-            )
-          })}
-        </Slider>
+    <section id="testimony" className="my-5 ">
+      <h1 className="text-center  text-3xl tracking-widest">TESTIMONY</h1>
+      <div>
+        <div className="max-w-xl mx-auto block text-center p-6 tracking-widest">
+          <Slider {...settings}>
+            {testimony.map((testi, index) => {
+              const { image, name, desc } = testi
+              return (
+                <div key={index} className="text-center">
+                  <img src={image} alt={name} className="w-56 block mx-auto rounded-full" />
+                  <p>{name}</p>
+                  <p>"{desc}"</p>
+                </div>
+              )
+            })}
+          </Slider>
+        </div>
       </div>
     </section>
   )
